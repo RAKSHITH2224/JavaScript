16fb4e0c1b659a2bfaa94eca3827aa1520bec637
RAKSHITH2224 <99284605+RAKSHITH2224@users.noreply.github.com>
2024-02-28 13:05:56 +0530

    DataType Comparisions

--- 1_basics/04_comparision_Datatypes.js
+++ 1_basics/04_comparision_Datatypes.js
@@ -8,10 +8,14 @@
 
 // console.log("2" > 1);
 
-console.log(null > 0);//comparision   //false
-console.log(null == 0);//equality check    //false
-console.log(null >= 0);    //true
+// console.log(null > 0);//comparision   //false
+// console.log(null == 0);//equality check    //false
+// console.log(null >= 0);    //true
 
-console.log(undefined == 0); //false
-console.log(undefined > 0);   //false
-console.log(undefined < 0);    //false
+// console.log(undefined == 0); //false
+// console.log(undefined > 0);   //false
+// console.log(undefined < 0);    //false
+
+
+
+// === This is strict check, which checks datatypes also
