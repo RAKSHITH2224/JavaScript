12c5c1fa544bb5e8630aec82fa051c8cf17a8109
RAKSHITH2224 <99284605+RAKSHITH2224@users.noreply.github.com>
2024-02-28 13:03:57 +0530

    DataType Comparisions

---
+++ 1_basics/04_comparision_Datatypes.js
@@ -0,0 +1,17 @@
+// console.log(2 > 1);
+// console.log(2 < 1);
+// console.log(2 >= 1);
+// console.log(2 <= 1);
+// console.log(2 == 1);
+//console.log(2 != 1);
+
+
+// console.log("2" > 1);
+
+console.log(null > 0);//comparision   //false
+console.log(null == 0);//equality check    //false
+console.log(null >= 0);    //true
+
+console.log(undefined == 0); //false
+console.log(undefined > 0);   //false
+console.log(undefined < 0);    //false
