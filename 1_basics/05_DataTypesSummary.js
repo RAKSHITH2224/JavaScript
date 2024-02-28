4b45535f48fc476c46ae1f7935f926f77f394ca4
RAKSHITH2224 <99284605+RAKSHITH2224@users.noreply.github.com>
2024-02-28 13:26:24 +0530

    summary on DataTypes

---
+++ 1_basics/05_DataTypesSummary.js
@@ -0,0 +1,46 @@
+// Premitive
+// 7 types
+// string, number, boolean, null, undefined, Symbol, BigInt
+
+const score = 100
+const scoreValue = 100.33
+
+const isLoggedIn = true
+
+const outsideTemp = null
+let userEmail; 
+
+const id = Symbol('123')
+const id2 = Symbol('123')
+
+
+console.log(id == id2);
+
+
+
+
+
+
+
+
+//Reference (Non premitive)
+//Types: Array, Objects, Functions 
+
+
+const heros = ["iron man","raksh","flash"]//array
+
+
+//objects look like below
+let myObject = {
+    name: "Rakshith",
+    age: 22
+}
+
+function myFunction(){
+    console.log("Hello")   
+}    //datatype is function
+
+
+
+
+
